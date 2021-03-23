7754c3b28df69db8df11e8963c89e7a0b964424b
reh42 <reharbison42@gmail.com>
2021-03-22 20:30:57 -0500

    part 2 almost complete

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class User {
+public class User extends AbstractEntity{
 
     private static final BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
     @NotNull
---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
@@ -0,0 +1,34 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class LoginFormDTO {
+
+    @NotNull
+    @NotBlank
+    @Size(min=3, max=50)
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min=6, max=50)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
